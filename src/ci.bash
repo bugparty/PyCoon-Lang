b02d70d2ccb972d066a740823e123a4f434feea8
Bowen Han <fancycode@gmail.com>
2024-01-16 08:48:40 +0000

    experiment gtest

---
+++ src/ci.bash
@@ -0,0 +1,5 @@
+
+make
+./onion ../doc/language_samples/test_cases/variable01.onion
+./onion ../doc/language_samples/test_cases/variable02.onion
+./onion ../doc/language_samples/test_cases/if01.onion
