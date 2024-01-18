5c9f675fc8f2d0c63f945d1d7351104a7ca43fcf
Bowen Han <fancycode@gmail.com>
2024-01-18 05:57:30 +0000

    seperate main function to main.cc

---
+++ src/flex_externs.hpp
@@ -0,0 +1,4 @@
+extern int yylex();
+extern int yyleng;
+extern FILE *yyin, *yyout;
+extern char *yytext;
---
+++ src/main.cc
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "flex_externs.hpp"
+int main(int argc,char** argv)
+{
+
+    ++argv, --argc;  /* skip over program name */
+    if ( argc > 0 )
+        yyin = fopen( argv[0], "r" );
+    else
+        yyin = stdin;
+
+    yylex();
+    return 0;
+}
