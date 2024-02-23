497bf641621f53af165f7dfbf433e37f28490c60
Bowen Han <fancycode@gmail.com>
2024-02-23 05:56:10 +0000

    add omited  code

--- src/code_node.hpp
+++ src/code_node.hpp
@@ -1,19 +1,21 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <iostream>
 #include "tok.h"
 struct CodeNode;
 typedef struct CodeNode CodeNode;
-union OnionVal{
+/*
+if target type is YYSYMBOL_arithmetic_op, store the temp variable name in val.str*/
+struct CodeNode{
+    std::string IRCode;
+    std::string sourceCode;
+    union OnionVal{
     int i;
     float f;
     double d;
     std::string* str;
-};
-struct CodeNode{
-    std::string IRCode;
-    std::string sourceCode;
-    OnionVal val;
+    }val;
     int type;
     int subType;
     std::vector<CodeNode*> children;
@@ -32,5 +34,8 @@ struct CodeNode{
     }
     void addChild(CodeNode* child){
         children.push_back(child);
+    }
+    void printIR(){
+        std::cout<<"IRCode:"<<std::endl << IRCode <<"end of IRCode"<<std::endl;
     }
 };
