90727f70d8d8a1890a2abcb5c5e2838baf4218b8
Bowen Han <fancycode@gmail.com>
2024-03-06 08:39:12 +0000

    add last test case

--- src/test_parser.sh
+++ src/test_parser.sh
@@ -19,4 +19,5 @@ fi
 #cat ../doc/language_samples/IR_test_case/2expr.onion | ./onion
 # cat tests/IR_auto_tests/function01.onion | ./onion -p
 #cat tests/IR_auto_tests/a_plus_b.onion | ./onion
-cat ../doc/language_samples/IR_test_case/while.onion | ./onion
+#cat ../doc/language_samples/IR_test_case/if03.onion | ./onion
+cat tests/IR_auto_tests/if03.onion | ./onion
