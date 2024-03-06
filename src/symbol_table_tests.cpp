9e776f6ee165f5c40232631f7b89c5c0c702ac3a
Bowen Han <fancycode@gmail.com>
2024-03-06 03:46:06 +0000

    add allocate_label

--- src/symbol_table_tests.cpp
+++ src/symbol_table_tests.cpp
@@ -7,10 +7,13 @@ int main(){
     cout << manager->allocate_temp(SymbolType::SYM_VAR_INT)<<endl;
     auto m2 = SymbolManager::getInstance();
     assert(m2 == manager);
-    auto* s0 = manager->find("_temp0");
+    auto* s0 = manager->find("_temp0","temp");
     assert(s0!=nullptr);
-    auto *s1 = m2->find("_temp0");
+    auto *s1 = m2->find("_temp0","temp");
     assert(s1!=nullptr);
     manager->addFunction("f",*new std::vector<Symbol*>());
+    for(int i = 0; i < 5; i++){
+        cout << "allocate label:"<< manager->allocate_label()<<endl;
+    }
     return 0;
 }
