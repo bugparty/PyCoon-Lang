2fe3e93f75e7d34c221ce783300f5386b65dfc85
Bowen Han <fancycode@gmail.com>
2024-03-07 23:58:40 +0000

    add example and support

--- src/states.cpp
+++ src/states.cpp
@@ -1,24 +1,56 @@
 #include "states.hpp"
 std::stack<std::string> functionStack;
-std::stack<std::string> loopStack;
+std::stack<CodeNode*> loopStack;
 std::stack<CodeNode*> codeNodeStack;
 
 std::string currentFunction(){
-    return functionStack.top();
+    if(functionStack.empty()){
+        return "";
+    }else{
+        return functionStack.top();
+    }
 }
-void pushFunction(std::string name){
+void pushFunction(const std::string& name){
     functionStack.push(name);
 }
 std::string  popFunction(){
-    std::string name = functionStack.top();
-    functionStack.pop();
-    return name;
+    if(functionStack.empty()){
+        return "";
+    }else{
+        std::string name = functionStack.top();
+        functionStack.pop();
+        return name;
+    }
+    
 }
 void push_code_node(CodeNode* node){
     codeNodeStack.push(node);
 }
 CodeNode* pop_code_node(){
-    CodeNode* node = codeNodeStack.top();
-    codeNodeStack.pop();
-    return node;
+    if(codeNodeStack.empty()){
+        return nullptr;
+    }else{
+        CodeNode* node = codeNodeStack.top();
+        codeNodeStack.pop();
+        return node;
+    }
+}
+CodeNode* currentLoopTag(){
+    if(loopStack.empty()){
+        return nullptr;
+    }else{
+        return loopStack.top();
+    }
+}
+void pushLoopTag(CodeNode* name){
+    loopStack.push(name);
+}
+CodeNode* popLoopTag(){
+    if(loopStack.empty()){
+        return nullptr;
+    }else{
+        auto  node = loopStack.top();
+        loopStack.pop();
+        return node;
+    }
 }
--- src/states.hpp
+++ src/states.hpp
@@ -1,12 +1,13 @@
 #include <stack>
 #include <string>
 #pragma once
-extern std::stack<std::string> functionStack;
-extern std::stack<std::string> loopStack;
 struct CodeNode;
 std::string currentFunction();
-void pushFunction(std::string name);
+void pushFunction(const std::string& name);
 std::string popFunction();
+CodeNode* currentLoopTag();
+void pushLoopTag(CodeNode* node);
+CodeNode* popLoopTag();
 
 void push_code_node(CodeNode* node);
 CodeNode* pop_code_node();
